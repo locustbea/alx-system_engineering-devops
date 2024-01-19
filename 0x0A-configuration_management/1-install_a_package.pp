e22728267ec80871761d62220bf68516022b6375
locustbea <locustbea@yahoo.com>
2024-01-19 06:56:14 -0800

    Install Packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-# install flask 2.1.0 from pip3
-package{'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',}
+# Install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
