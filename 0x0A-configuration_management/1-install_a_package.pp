ad09b8000646812a6fbe007a3f182c67682ba3b8
locustbea <locustbea@yahoo.com>
2024-01-26 06:46:59 -0800

    Install Package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,5 @@
-#!/usr/bin/puppet
-# Install from specific Python version
-package { 'python':
- ensure => '3.8.10',
- provider => 'pip3',
-}
-# Install a specific version of flask (2.1.0)
-package { 'Flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
-}
-# Install Wekzeug v2.1.1
-package { 'Werkzeug':
-  ensure => '2.1.1',
-  provider => 'pip3',
-  require => Package['Flask'],
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
