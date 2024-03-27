64485990c1080338b3e20525b6c517ae9c8e4574
root <you@example.com>
2024-03-27 04:56:14 -0700

    0 sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,15 +4,32 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """get number of subscribers to a certain subreddit """
+    client_id = "PpEfMsSxFbxQHsIMzr5faQ"
+    client_secret = "8yTk0bJrYf78NPzdONjt1zWRmYo8SQ"
+    r_usr = "tarekElaasri"
+    r_pass = "(k4r9CQ^^?wN46L"
+    url = "https://www.reddit.com/api/v1/access_token"
+    auth_data = {"grant_type": "password", "username": r_usr,
+                 "password": r_pass}
+    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+    header = {'user-agent':
+              'script:subscriber_counter_v0.1 by /u/tarekElaasri'}
+    res = requests.post(url, data=auth_data,
+                        headers=header,
+                        auth=auth)
+
+    if res.status_code == 200:
+        token = res.json()['access_token']
+    else:
+        print(res)
+
+    url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+    headers = {'Authorization': 'bearer ' + token,
+               'User-Agent':
+               'script:subscriber_counter_v0.1 by /u/tarekElaasri'}
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
-
-
